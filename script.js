36bf312852693cec57b2dd0363a3f3ecd6f22cd5
Andre Rodriguez <drae7299@gmail.com>
2021-06-15 19:45:17 -0400

    adding in more button functionality to append in the inner html on click

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ var userHistoryContainer = document.querySelector('#history')
 //event listerner on submit
 function populateUserHistory() {
     userHistoryContainer.innerHTML = "";
+//loop through recent search to give most recent 
+//also added in the ability for the on click button to access the container to append into the innner html
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
 } 
 //fuction to render history 
 //function to put fetch data on the page
