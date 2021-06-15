bc417cd05516083336bfe4d3fa7dfd347f65e8b0
Andre Rodriguez <drae7299@gmail.com>
2021-06-15 19:43:16 -0400

    added in base function for on click feature to return the users search value

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ var weatherKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
 var nowContainer = document.querySelector('#now');
-//event listerner on submit, 
+var forecastContainer = document.querySelector('#forecast');
+var userHistoryContainer = document.querySelector('#history')
+//event listerner on submit
+function populateUserHistory() {
+    userHistoryContainer.innerHTML = "";
+} 
 //fuction to render history 
 //function to put fetch data on the page
 
