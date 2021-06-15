0f1f5bbec11151302f68b53f3bede9cb6514b6be
Andre Rodriguez <drae7299@gmail.com>
2021-06-15 19:38:39 -0400

    replaced html with 1st draft

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //global variables 
 var userHistory = [];
 var weatherApi = 'https://api.openweathermap.org';
+var weatherKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+
+
+//elements to append to dom
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var nowContainer = document.querySelector('#now');
 //event listerner on submit, 
 //fuction to render history 
 //function to put fetch data on the page
